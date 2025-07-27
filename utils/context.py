40496670504c3067887c9cce13f3bedd30c66a0c
Ankur <ankurb9@gmail.com>
2025-07-27 14:26:05 +0530

    Commiting changes

--- utils/context.py
+++ utils/context.py
@@ -13,25 +13,20 @@ class Context:
     username: str = field(default="", init=False, repr=False)
     password: str = field(default="", init=False, repr=False)
     api_host: str = field(default="", init=False, repr=False)
-    project_root: str = field(default="", init=False, repr=False)
+    project_root: str = field(default_factory=lambda: str(Path(__file__).resolve().parents[1]), init=False, repr=False)
 
     def __post_init__(self):
         if not self.env:
             raise ValueError("Env cannot be blank")
-
         self._set_context()
 
     def _set_context(self):
-
-        project_root = Path(__file__).resolve().parents[1]
-        config_file = project_root / "config" / f"{self.env}.yaml"
-
-        with config_file.open() as f:
+        config_file = f"{self.project_root}/config/{self.env}.yaml"
+        with open(config_file) as f:
             conf = yaml.safe_load(f.read())
 
             self.url = conf["url"]
             self.username = conf["username"]
             self.password = conf["password"]
             self.api_host=conf["host"]
-            self.project_root = self.project_root = str(Path(__file__).resolve().parents[2])
 

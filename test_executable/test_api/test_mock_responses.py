66a734cdcf7a9aca33da77acafd6e6bad2cb0906
Ankur <ankurb9@gmail.com>
2025-08-15 10:05:25 +0530

    Removed repr from context

--- test_executable/test_api/test_mock_responses.py
+++ test_executable/test_api/test_mock_responses.py
@@ -4,12 +4,12 @@ import requests
 @responses.activate
 def test_mock_api():
     responses.add(
-        method=responses.POST,
+        method="POST",
         url="https://api.example.com/users",
         json={"id": 1, "name": "Mocked"},
         status=201,
     )
 
-    resp = requests.post("https://api.example.com/users", json={"name": "ankur"})
+    resp = requests.post("https://api.example.com/users", json=None)
     assert resp.status_code == 201
     assert resp.json() == {"id": 1, "name": "Mocked"}
--- utils/api_utils.py
+++ utils/api_utils.py
@@ -2,11 +2,14 @@ import json
 from typing import Literal, Optional, Union, Any
 from urllib.parse import urljoin
 from jsonpath_ng import parse
-from playwright.sync_api import APIResponse, Playwright
-from utils import ctx
+from utils import ctx, log
+import requests
+import responses
 
 def __get_json_body(body):
-
+    if not body:
+        log.error("Empty payload provided.")
+        raise Exception("Empty payload provided.")
     if isinstance(body, str):
 
         if body.endswith(".json"):
@@ -21,40 +24,57 @@ def __get_json_body(body):
     return None
 
 
-def request(playwright: Playwright, url: str,
+def request( url: str,
             method: Literal["GET", "POST", "PUT", "DELETE"],
             body: Optional[Union[str, dict]] = None,
-            status_code_expected: int = 200) -> Any | None:
+            status_code_expected: int = 200,
+            mock: bool=False,
+            response_json: Optional[Union[str, dict]]= None
+            ) -> Any | None:
+
 
     final_url = urljoin(ctx.api_host, url=url)
-    data = __get_json_body(body)
+    data = None
+    if method not in ["GET", "DELETE"]:
+        data = __get_json_body(body)
+
     headers = {"Content-Type": "application/json",
                "x-api-key": "reqres-free-v1"}
 
-    req = playwright.request.new_context(base_url=ctx.api_host, extra_http_headers=headers)
+    if mock:
+        res: requests.Response= mock_response(url="/api/users",
+                            method="POST",
+                            body=data,
+                            status_code_expected=201)
+    else:
+        res: requests.Response = requests.request(url=final_url,
+                                                  method=method,
+                                                  headers=headers,
+                                                  data=data)
 
-    method_func = {
-        "GET": req.get,
-        "POST": req.post,
-        "PUT": req.put,
-        "DELETE": req.delete
-    }[method]
-
-    request_args = {
-        "url": final_url,
-        "headers": headers,
-        "max_retries": 5
-    }
-    if method not in ["GET", "DELETE"]:
-        request_args["data"] = data
-
-    res: APIResponse = method_func(**request_args)
-
-    assert res.status == status_code_expected, f"Invalid status code received: {res.status} - {res.status_text}."
+    assert res.status_code == status_code_expected, f"Invalid status code received: {res.status_code} - {res.text}."
     try:
         return res.json()
     except:
         return None
+
+@responses.activate
+def mock_response(url: str,
+                    method: Literal["GET", "POST", "PUT", "DELETE"],
+                    body: Optional[Union[str, dict]] = None,
+                    status_code_expected: int = 200,
+                    response_json: Optional[Union[str, dict]]= None
+            ) -> Any | None:
+
+    responses.add(
+        method=method,
+        url=url,
+        json=response_json,
+        status=status_code_expected,
+    )
+
+    return requests.request(url="https://api.example.com/users", method=method, json=body)
+
 
 
 def parse_json(json_body, path) -> list:

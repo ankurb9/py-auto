bf231fc56fb552ebc4346a02243baf1b58f638d8
Ankur <ankurb9@gmail.com>
2025-08-23 20:49:27 +0530

    Removed repr from context

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import glob
+from email.policy import default
+
 import pandas as pd
 from utils import ctx
 import os
@@ -17,6 +19,13 @@ def pytest_addoption(parser):
         help="Set the logging level (DEBUG, INFO, WARNING, etc.)"
     )
 
+    parser.addoption("--env", action="store", default="qa1", help="Provide the environment")
+
+    parser.addoption("--suite",
+                     action="store",
+                     default=None,
+                     help="Provide the enablement suite or ite will be picked from enablement folder.")
+
 def pytest_configure(config):
     env = config.getoption("--env")
     log_level = config.getoption("--loglevel")
@@ -28,30 +37,33 @@ def pytest_collection_modifyitems(session, config: Config, items):
     marker = config.getoption("-m", "regression")
     apps = config.getoption("-k")
     test_df = pd.DataFrame()
-    if apps:
-        app_list = [a.strip() for a in apps.split(",")]
-        test_df = pd.concat([pd.read_csv(f"{ctx.project_root}/enablement/{a}.csv") for a in app_list])
+    if config.getoption("--suite"):
+        pd.DataFrame(config.getoption("--suite"))
     else:
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        path = f"{script_dir}/enablement/*.csv"
-        path_list = glob.glob(path)
-        if path_list:
-            test_df = pd.concat([pd.read_csv(p) for p in path_list])
+        if apps:
+            app_list = [a.strip() for a in apps.split(",")]
+            test_df = pd.concat([pd.read_csv(f"{ctx.project_root}/enablement/{a}.csv") for a in app_list])
+        else:
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            path = f"{script_dir}/enablement/*.csv"
+            path_list = glob.glob(path)
+            if path_list:
+                test_df = pd.concat([pd.read_csv(p) for p in path_list])
 
-    if marker:
-        if marker not in test_df.columns:
-            raise Exception("Marker not found in csv files provided under enablement")
+        if marker:
+            if marker not in test_df.columns:
+                raise Exception("Marker not found in csv files provided under enablement")
 
-        test_df = test_df.astype(str)
-        exec_df = test_df[test_df[marker].str.lower() == "true"]
-        exec_df['modules'] = exec_df['modules'].str.replace(".py", "")
-        exec_df["combined"] = exec_df['modules'] + '::' + exec_df['tests']
+            test_df = test_df.astype(str)
+            exec_df = test_df[test_df[marker].str.lower() == "true"]
+            exec_df['modules'] = exec_df['modules'].str.replace(".py", "")
+            exec_df["combined"] = exec_df['modules'] + '::' + exec_df['tests']
 
-        for item in items:
+            for item in items:
 
-            module_name = str(item.module.__name__).split(".")[-1]
-            test_name = str(item.name).split("[")[0]
+                module_name = str(item.module.__name__).split(".")[-1]
+                test_name = str(item.name).split("[")[0]
 
-            if module_name in exec_df["modules"].values and test_name in exec_df["tests"].values:
-                item.add_marker(marker)
+                if module_name in exec_df["modules"].values and test_name in exec_df["tests"].values:
+                    item.add_marker(marker)
 
